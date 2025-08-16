9eb3fadcbb453d2c172daa588023e98adbc52881
Salvius10 <melvinsalvius.i@gmail.com>
2025-08-16 18:40:33 +0530

    dashboard

--- expense/src/pages/Dashboard.jsx
+++ expense/src/pages/Dashboard.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Dashboard = () => {
+    const [transaction,setTransaction]=useState([{title:"",date:"",status:"",amount:0}])
+    const [showForm,setShowForm]=useState(false)
+    const [formData,setFormData]=useState([{title:"",data:"",status:"",amount:""}])
+
+    const handleAddTransaction=()=>{
+        setTransaction([...transaction,formData])
+        setFormData({title:"",data:"",status:"",amount:""})
+        setShowForm(false)
+    }
+
+    const handleDelete=(index)=>{
+        setTransaction(transaction.filter((_,i)=>i!==index))
+    }
+
   return (
     <div className='flex bg-black h-screen' style={{ fontFamily: 'Inter, sans-serif' }}>
         <div className='w-64 text-[#555555]'>
@@ -54,20 +68,52 @@ const Dashboard = () => {
                             <th className='border border-white border-solid px-2 py-2'>Date</th>
                             <th className='border border-white border-solid px-2 py-2'>Status</th>
                             <th className='border border-white border-solid px-2 py-2'>Amount</th>
+                            <th className="border border-white border-solid px-2 py-2">Actions</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td className='border border-solid px-2 py-2'>Food</td>
-                            <td className='border border-solid px-2 py-2'>29-04-2019</td>
-                            <td className='border border-solid px-2 py-2'>Paid</td>
-                            <td className='border border-solid px-2 py-2'>1500</td>
-                        </tr>
+                        {transaction.map((ele,idx)=>(
+                            <tr key={idx}>
+                                <td className='border border-solid px-2 py-2'>{ele.title}</td>
+                                <td className='border border-solid px-2 py-2'>{ele.date}</td>
+                                <td className='border border-solid px-2 py-2'>{ele.status}</td>
+                                <td className='border border-solid px-2 py-2'>{ele.amount}</td>
+                                <td className='border border-solid px-2 py-2'>
+                                    <button onClick={() => handleDelete(idx)} className="bg-red-600 text-white px-3 py-1 rounded hover:bg-red-800">Delete</button>
+                                </td>
+                            </tr>
+                        ))}
+
                     </tbody>
                 </table>
                 <div>
-                    <button className='text-white bg-[#582688] border border-solid border-white px-6 py-2 mt-4 rounded '>+ADD</button>
+                    <button className='text-white bg-[#582688] border border-solid border-white px-6 py-2 mt-4 rounded ' onClick={()=>setShowForm(!showForm)}>+ADD</button>
                 </div>
+                {showForm && (
+                    <div>
+                        <input 
+                        type="text"
+                        placeholder='title'
+                        value={formData.title}
+                        onChange={(e)=>setFormData({...formData,title:e.target.value})} />
+                        <input 
+                        type="date"
+                        placeholder='date'
+                        value={formData.date}
+                        onChange={(e)=>setFormData({...formData,date:e.target.value})} />
+                        <select value={formData.status} onChange={(e)=>setFormData({...formData,status:e.target.value})}>
+                            <option value="">Select</option>
+                            <option value="Paid">Paid</option>
+                            <option value="Pending">Pending</option>
+                        </select>
+                        <input 
+                        type="number"
+                        placeholder='Amount'
+                        value={formData.amount}
+                        onChange={(e)=>setFormData({...formData,amount:e.target.value})} />
+                        <button onClick={handleAddTransaction}>Save</button>
+                    </div>
+                )}
             </div>
         </div>
     </div>
