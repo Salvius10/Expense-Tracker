c39717f85720486f90bbfd1125a6b272b273a5a0
Salvius10 <melvinsalvius.i@gmail.com>
2025-08-18 14:27:32 +0530

    lol

--- expense/src/App.jsx
+++ expense/src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import Dashboard from './pages/Dashboard'
 import './App.css'
 import Income from './pages/Income'
+import Expense from './pages/Expense'
 function App() {
 
   return (
-    <Income/>
+    <Expense/>
   )
 }
 
